415d33e7f73aadd39df41061930d1aa15484492e
Rodrigo Vedovato <vedovato.rodrigo@gmail.com>
2020-08-28 16:14:27 -0300

    Removing tdb loader

--- fuseki-singledb/entrypoint.sh
+++ fuseki-singledb/entrypoint.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-echo "Preloading database..."
-java -cp ./fuseki-server.jar tdb.tdbloader --tdb=$DB_CONFIG $DB_FILE
-
 echo "Indexing triples..."
 java -cp ./fuseki-server.jar jena.textindexer --desc=$DB_CONFIG
 
