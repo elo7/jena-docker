9712382c27dc6d12dd2b7dbecc610a7a3ff56b8b
Rodrigo Vedovato <vedovato.rodrigo@gmail.com>
2020-08-28 10:41:53 -0300

    indexando dados no entrypoint

--- fuseki-singledb/entrypoint.sh
+++ fuseki-singledb/entrypoint.sh
@@ -2,4 +2,10 @@
 
 set -e
 
-$FUSEKI_HOME/fuseki-server --file=$DB_FILE "/$DB_NAME"
+echo "Preloading database..."
+java -cp ./fuseki-server.jar tdb.tdbloader --tdb=$DB_CONFIG $DB_FILE
+
+echo "Indexing triples..."
+java -cp ./fuseki-server.jar jena.textindexer --desc=$DB_CONFIG
+
+$FUSEKI_HOME/fuseki-server --conf=$DB_CONFIG
